dccf3512badcff440b4a19072bca4f30ab8964c4
progLime <93077970+progLime@users.noreply.github.com>
2022-11-07 17:12:47 +0200

    Creation of data models

---
+++ InformationRetrieval/Models/BookRating.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+using System;
+
+#nullable disable
+
+namespace InformationRetrieval
+{
+    /// <summary>
+    /// The class describes a row of the book's ratings CSV
+    /// <summary>
+    [ElasticsearchType(IdProperty = nameof(Id))]
+    public class BookRating
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The unique id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The rating
+        /// </summary>
+        public double Rating { get; set; }
+
+        /// <summary>
+        /// The user's id
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// The book's id
+        /// </summary>
+        public string BookId { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default Constructor 
+        /// </summary>
+
+        public BookRating()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ InformationRetrieval/Models/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+
+#nullable disable
+
+namespace InformationRetrieval
+{
+    /// <summary>
+    /// The class describes a row of the user's CSV
+    /// <summary>
+    
+    public class User
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The location
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// The age
+        /// </summary>
+        public float? Age { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default Constructor 
+        /// </summary>
+
+        public User()
+        {
+
+        }
+
+        #endregion
+    }
+}
+
